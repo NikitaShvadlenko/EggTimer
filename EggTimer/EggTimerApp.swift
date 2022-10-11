894f3bd7930a65bb9293b1ed662c5085a96ceea7
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-10-12 05:58:21 +1300

    Initial Commit

---
+++ EggTimer/EggTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggTimerApp.swift
+//  EggTimer
+//
+//  Created by Nikita Shvad on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct EggTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
