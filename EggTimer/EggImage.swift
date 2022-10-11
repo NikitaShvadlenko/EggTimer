224278ca7ea080b0af04bbbcc0fb976a4f051b85
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-10-12 12:44:28 +1300

    EggImage file

--- EggTimer/EggImage.swift
+++ EggTimer/EggImage.swift
@@ -1,21 +1,56 @@
-//
-//  EggImage.swift
-//  EggTimer
-//
-//  Created by Nikita Shvad on 12.10.2022.
-//  Copyright © 2022 Nikita Shvadlenko. All rights reserved.
-//
-
 import SwiftUI
 
 struct EggImage: View {
+
+    var egg: Egg
+    @State private var opacity = 1.0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            eggImage
+                .opacity(opacity)
+
+            Button {
+                withAnimation(fadeInFadeOutAnimation()) {
+                    opacity -= 0.4
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                    opacity = 1
+                }
+            } label: {
+                Text(egg.name)
+                    .fontWeight(.heavy)
+                    .frame(height: 100)
+                    .font(.title)
+                    .lineLimit(1)
+                    .foregroundColor(Color(Asset.Color.eggStyleText.color))
+                    .aspectRatio(contentMode: .fit)
+                    .minimumScaleFactor(0.4)
+                    .opacity(opacity)
+            }
+            .opacity(opacity)
+        }
+    }
+
+     var eggImage: some View {
+        Image(egg.imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+    }
+
+     var fadeInFadeOutAnimation = {
+         Animation.linear(duration: 0.3)
+            .repeatCount(2, autoreverses: true)
     }
 }
 
 struct EggImage_Previews: PreviewProvider {
     static var previews: some View {
-        EggImage()
+        EggImage(
+            egg: Egg.init(
+                name: L10n.hard,
+                imageName: Asset.hardEgg.name
+            )
+        )
     }
 }
