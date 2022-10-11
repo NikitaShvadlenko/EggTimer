ab393447f3a06156504e316287e25f3869e6a0a2
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-10-12 12:43:30 +1300

    Assest

---
+++ EggTimer/EggImage.swift
@@ -0,0 +1,21 @@
+//
+//  EggImage.swift
+//  EggTimer
+//
+//  Created by Nikita Shvad on 12.10.2022.
+//  Copyright © 2022 Nikita Shvadlenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct EggImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EggImage_Previews: PreviewProvider {
+    static var previews: some View {
+        EggImage()
+    }
+}
---
+++ EggTimer/Model/Egg.swift
@@ -0,0 +1,9 @@
+//
+//  EggModel.swift
+//  EggTimer
+//
+//  Created by Nikita Shvad on 12.10.2022.
+//  Copyright © 2022 Nikita Shvadlenko. All rights reserved.
+//
+
+import Foundation
